3cf7c8539b1203f2d6f72a1cc7feaec36e921a39
Gradi Mbuyi <gradimbuyi@outlook.com>
2023-02-23 23:54:40 -0500

    Made changes to how the Chess board is generated

--- src/Chessboard.java
+++ src/Chessboard.java
@@ -1,42 +1,77 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
-public class Chessboard extends JFrame {
+public class Chessboard {
+    /* Variables for our Chess board */
+    private JFrame frame;
     private ChessTile boardTiles[][];
+
+    /* Stores Pieces information */
     private String boardState;
+    private Image pieceImage[];
+    private static Color light = Color.decode("#fafbfc");
+    private static Color dark = Color.decode("#5b80ba");
 
     /* Constructor to generate our chess board */
     public Chessboard () throws HeadlessException {
         this.boardTiles = new ChessTile[8][8];
-
-        /* The state of the game will be represented using FEN notation */
         this.boardState = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
-        setTitle("Chess By Gradi");
-        setSize(1080, 1108);
+        this.frame = new JFrame();
+        this.frame.setTitle("Chess");
+        this.frame.setSize(1080, 1108);
+        this.frame.setDefaultCloseOperation(3);
 
         generateChessBoard(boardTiles);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true); setResizable(false);
+
+        this.frame.setVisible(true);
+        this.frame.setResizable(false);
+        this.frame.setLocationRelativeTo(null);
     }
 
-    /* Colors our chess tiles */
-    private void generateChessBoard(ChessTile boardTiles[][]){
+    /* Function to draw our chess board */
+    private void generateChessBoard(ChessTile boardTiles[][]) {
+        Color tileColor;
 
-        /* The color value "true" represent a light/white tile */
-        boolean color = true;
-
+        /* Chess boards are represented using 2d arrays, hence why we have a for loop within another */
         for(int locationX = 0; locationX < 8; locationX++) {
             for(int locationY = 0; locationY < 8; locationY++) {
-                ChessTile tiles = new ChessTile(locationX, locationY, color);
 
-                if(color == true) tiles.setBackground(Color.decode("#fafbfc"));
-                else tiles.setBackground(Color.decode("#5b80ba"));
+                /* Sets the color of our chess tile */
+                if((locationX % 2) == (locationY % 2)) tileColor = light;
+                else tileColor = dark;
+
+                /* Creates an instance of our chess tile */
+                ChessTile tiles = new ChessTile(locationX, locationY, tileColor);
+
+                /* Adds our tile to our frame */
+                tiles.setBackground(tileColor);
                 tiles.setBounds(locationX * 135, locationY * 135, 135, 135);
-                boardTiles[locationX][7 - locationY] = tiles; add(tiles); color = !color;
+                boardTiles[locationX][7 - locationY] = tiles;
+                this.frame.add(tiles);
             }
-            color = !color;
         }
+    }
+
+    /* Function to generate our pieces and draw them on our board */
+    private void generatePieces() throws IOException {
+        this.pieceImage = new Image[12];
+
+        pieceImage[0] = ImageIO.read(new File("../images/black_king.png"));
+        pieceImage[1] = ImageIO.read(new File("../images/black_queen.png"));
+        pieceImage[2] = ImageIO.read(new File("../image/black_rook.png"));
+        pieceImage[3] = ImageIO.read(new File("../image/black_bishop.png"));
+        pieceImage[4] = ImageIO.read(new File("../image/black_knight.png"));
+        pieceImage[5] = ImageIO.read(new File("../image/black_pawn.png"));
+        pieceImage[6] = ImageIO.read(new File("../image/white_king.png"));
+        pieceImage[7] = ImageIO.read(new File("../image/white_queen.png"));
+        pieceImage[8] = ImageIO.read(new File("../image/white_rook.png"));
+        pieceImage[9] = ImageIO.read(new File("../image/white_bishop.png"));
+        pieceImage[10] = ImageIO.read(new File("../image/white_knight.png"));
+        pieceImage[11] = ImageIO.read(new File("../image/white_pawn.png"));
     }
 
     public ChessTile[][] getBoardTiles() {
